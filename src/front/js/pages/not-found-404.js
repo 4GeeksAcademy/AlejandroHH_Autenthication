1c6cbe0573fd488b5cd08894e279773a16aa93e9
Alejandro Hipólito Hernando <126016967+Alejandro-Hipolito@users.noreply.github.com>
2023-06-12 17:12:23 +0000

    ...

--- src/front/js/pages/not-found-404.js
+++ src/front/js/pages/not-found-404.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const NotFound404 = () => {
   return (
     <div className='d-flex justify-content-center align-items-center text-center '>
-        <h1 style={{color: 'white'}}>NOT FOUND! 404</h1>
+        <h1 style={{color: 'white'}}>NOT FOUND! 404!</h1>
     </div>
   )
 }
